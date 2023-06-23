e83c4897da2cad3d355e9963e4bfdfcd57b494a2
HamzaR19 <m.hamza.rahman@gmail.com>
2023-06-22 23:16:07 -0500

    more changes

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,13 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    
+    department_id INT,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+    ON DELETE NULL
+);
+
+
+CREATE TABLE 
 
 
